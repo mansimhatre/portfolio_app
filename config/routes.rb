d841b12690b1edffd68ef133b9089c92e3da53fd
unknown <Administrator@celab2-4.comp1-domain.spit.ac.in>
2012-09-16 15:10:43 +0530

    updated the home page

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ PortfolioApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'companies#index'
 
   # See how all your routes lay out with "rake routes"
 
