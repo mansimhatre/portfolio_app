1286e633283b6185198153cc8e50d902e507a77e
unknown <Administrator@celab2-4.comp1-domain.spit.ac.in>
2012-09-16 14:58:30 +0530

    checked no. of symbols

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ActiveRecord::Base
+  validate:symbol_three_or_four_letters
+   def   symbol_three_or_four_letters
+     if(symbol.length>4 or symbol.length<3)
+     errors.add :symbol,'should be 3 or 4' end
+
+   end
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,20 @@
 class Investment < ActiveRecord::Base
+
+
+  validate :cost_should_be_positive
+
+   def cost_should_be_positive
+     if (cost<0)
+       errors.add :cost,'Should be positive'
+     end
+   end
+  validate :quantity_should_be_positive
+
+  def  quantity_should_be_positive
+
+    if (quantity<0)
+      errors.add :quantity,'Should be positive'
+    end
+  end
+
 end
